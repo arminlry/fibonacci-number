1e4f8a24dc596d0b2c4a2c1df2dfbfa0234c5a76
arminlry <arminlry@DESKTOP-H8CJ8C9>
2023-10-31 22:35:48 +0330

    Add project files.

---
+++ fibonacci number/Program.cs
@@ -0,0 +1,29 @@
+﻿// armin liryaee
+// fibonacci number
+// tamrin 4
+char y = ' ';
+long n = 0;
+
+long b = 1;
+do
+{
+Console.WriteLine("enter N number:");
+
+long N = Convert.ToInt64(Console.ReadLine());
+
+        for (long c = 0 ; c < N ; c++)
+        {
+            long q = c + b;
+
+        Console.WriteLine(y);
+
+        Console.WriteLine(q);
+
+            n = b;
+
+            b = q;
+        }
+  
+    Console.WriteLine("do you want to continue?");
+
+} while (Console.ReadLine() == "y");
